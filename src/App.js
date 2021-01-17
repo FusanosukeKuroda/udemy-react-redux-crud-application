582b57bc56c6a2ac696720493908863527ed5d0b
fusanosuke.kuroda <fmh.k23@gmail.com>
2021-01-17 13:35:36 +0900

    change message hello-world

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload hello world:i.
-        </p>
+          Edit <code>src/App.js</code> hello world
+	  </p>
         <a
           className="App-link"
           href="https://reactjs.org"
